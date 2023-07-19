6a6fdd028ba86ce43861d7fe7d3f2e823def0880
u_alex90 <u_alex90@mail.ru>
2023-07-19 20:51:40 +1000

    fixed tests fo class Channel

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -3,4 +3,5 @@ from src.channel import Channel
 
 def test_channel():
     channel = Channel('UCwHL6WHUarjGfUM_586me8w')
-    assert channel.channel_id == 'UCwHL6WHUarjGfUM_586me8w'    assert isinstance(channel.info, dict)
+    assert channel.channel_id == 'UCwHL6WHUarjGfUM_586me8w'
+    assert isinstance(channel.info, dict)
