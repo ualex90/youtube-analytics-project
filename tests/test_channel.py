2e643a6ac98cc10fbb732920a2ce617632063486
u_alex90 <u_alex90@mail.ru>
2023-07-21 20:42:02 +1000

    Revert "Add comment in test"
    This reverts commit a7f83d4b87324ca8b844ddcce74c079d34fa2ab0.

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -2,8 +2,6 @@ from src.channel import Channel
 
 
 def test_channel():
-    # Test case 1 # channel_id
     channel = Channel('UCwHL6WHUarjGfUM_586me8w')
     assert channel.channel_id == 'UCwHL6WHUarjGfUM_586me8w'
-    # Test case 2 # info
     assert isinstance(channel.info, dict)
